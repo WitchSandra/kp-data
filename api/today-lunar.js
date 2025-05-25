bd8a6927bbfef7d1d4f03edfc59e8f52e7688a80
Witch Sandra <witchsandra96@gmail.com>
2025-05-25 17:31:05 +0300

    Update today-lunar.js

--- api/today-lunar.js
+++ api/today-lunar.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
 
   const data = await response.json();
 
-  const today = new Date().toISOString().split('T')[0];
+  const today = "2025-05-26";
 
   const todayData = data.find(entry => entry.date === today);
 
